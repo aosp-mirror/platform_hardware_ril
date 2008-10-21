dbbb392e15b5ace6f19e76c49c80ea14292e8a4d
The Android Open Source Project <initial-contribution@android.com>
2008-10-21 07:00:00 -0700

    Initial Contribution

---
+++ rild/Android.mk
@@ -0,0 +1,34 @@
+# Copyright 2006 The Android Open Source Project
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	rild.c
+
+
+LOCAL_SHARED_LIBRARIES := \
+	libcutils \
+	libril
+
+LOCAL_CFLAGS := -DRIL_SHLIB
+
+LOCAL_MODULE:= rild
+
+include $(BUILD_EXECUTABLE)
+
+# For radiooptions binary
+# =======================
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	radiooptions.c
+
+LOCAL_SHARED_LIBRARIES := \
+	libcutils \
+
+LOCAL_CFLAGS := \
+
+LOCAL_MODULE:= radiooptions
+
+include $(BUILD_EXECUTABLE)
