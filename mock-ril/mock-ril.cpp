9bc3e60491a0f7f66ceccd76bf634f42f02d6515
Wink Saville <wink@google.com>
2010-06-16 15:33:54 -0700

    Test how to send a "request" to JS and "print" messages to Android log.
    Change-Id: Icf56b12c998096c1574c55cdd5bba17b369920a1

--- mock-ril/mock-ril.cpp
+++ mock-ril/mock-ril.cpp
@@ -120,48 +120,32 @@ static const char * getVersion(void)
     return MOCK_RIL_VER_STRING;
 }
 
-static void * mainLoop(void *param)
-{
-
-    // Test using STLport
-    std::queue<void *> q;
-
-    LOGD("before push q.size=%d", q.size());
-    q.push(param);
-    LOGD("after push q.size=%d", q.size());
-    void *p = q.front();
-    if (p == param) {
-        LOGD("q.push succeeded");
-    } else {
-        LOGD("q.push failed");
-    }
-    q.pop();
-    LOGD("after pop q.size=%d", q.size());
-
-    // Test a simple protobuf
-    LOGD("create T");
-    T* t = new T();
-    LOGD("set_id(1)");
-    t->set_id(1);
-    int id = t->id();
-    LOGD("id=%d", id);
-    delete t;
-
-    LOGD("mainLoop E");
-
-    for (int i=0;;i++) {
-        sleep(10);
-        LOGD("mainLoop: looping %d", i);
-    }
-
-    LOGD("mainLoop X");
-
-    return NULL;
-}
 
 // Extracts a C string from a V8 Utf8Value.
 const char* ToCString(const v8::String::Utf8Value& value) {
   return *value ? *value : "<string conversion failed>";
+}
+
+// A javascript print function
+v8::Handle<v8::Value> Print(const v8::Arguments& args) {
+    bool first = true;
+    const int str_size = 1000;
+    char* str = new char[str_size];
+    int offset = 0;
+    for (int i = 0; i < args.Length(); i++) {
+        v8::HandleScope handle_scope;
+        if (first) {
+            first = false;
+        } else {
+            offset += snprintf(&str[offset], str_size, " ");
+        }
+        v8::String::Utf8Value strUtf8(args[i]);
+        const char* cstr = ToCString(strUtf8);
+        offset += snprintf(&str[offset], str_size, "%s", cstr);
+    }
+    LOGD("%s", str);
+    delete [] str;
+    return v8::Undefined();
 }
 
 // Report an exception
@@ -185,7 +169,7 @@ void LogErrorMessage(v8::Handle<v8::Message> message, const char *alternate_mess
     // Print location information under source line
     int start = message->GetStartColumn();
     int end = message->GetEndColumn();
-    char *error_string = new char[end];
+    char *error_string = new char[end+1];
     memset(error_string, ' ', start);
     memset(&error_string[start], '^', end - start);
     error_string[end] = 0;
@@ -211,38 +195,136 @@ void ReportException(v8::TryCatch* try_catch) {
     LogErrorMessage(msg, ToCString(exception));
 }
 
-void testV8() {
-    LOGD("testV8 E:");
+bool callOnRequest(v8::Handle<v8::Context> context, int param) {
     v8::HandleScope handle_scope;
     v8::TryCatch try_catch;
 
-    // Catch errors as they occur as using ReportException doesn't work??
+    // Get the onRequestFunction, making sure its a function
+    v8::Handle<v8::String> name = v8::String::New("onRequest");
+    v8::Handle<v8::Value> onRequestFunctionValue = context->Global()->Get(name);
+    if(!onRequestFunctionValue->IsFunction()) {
+        // Wasn't a function
+        LOGD("callOnRequest X wasn't a function");
+        return false;
+    }
+    v8::Handle<v8::Function> onRequestFunction =
+        v8::Handle<v8::Function>::Cast(onRequestFunctionValue);
+
+    // Create the parameter
+    v8::Handle<v8::Value> v8Param = v8::Number::New(param);
+
+    // Invoke onRequest
+    bool retValue;
+    const int argc = 1;
+    v8::Handle<v8::Value> argv[argc] = { v8Param };
+    v8::Handle<v8::Value> result = onRequestFunction->Call(context->Global(), argc, argv);
+    if (try_catch.HasCaught()) {
+        ReportException(&try_catch);
+        retValue = false;
+    } else {
+        v8::String::Utf8Value result_string(result);
+        LOGD("callOnRequest result=%s", ToCString(result_string));
+        retValue = true;
+    }
+    return retValue;
+}
+
+// There appears to be a bug which causes TryCatch.HasCaught()
+// to return true after creating a String Value.
+void v8Bug1() {
+    v8::HandleScope handle_scope;
+    v8::TryCatch try_catch;
+
+    LOGD("v8Bug1: start, HasCaught()=%d", try_catch.HasCaught());
+
+    v8::Handle<v8::Value> mydata(v8::String::New("hello"));
+    LOGD("v8Bug1 after creating mydata, HasCaught()=%d", try_catch.HasCaught());
+    if (try_catch.HasCaught()) {
+        LOGD("v8Bug1: Why is HasCaught true");
+    }
+}
+
+void testV8() {
+    LOGD("testV8 E:");
+    v8::HandleScope handle_scope;
+
+    // Add a Message listner to Catch errors as they occur
     v8::Handle<v8::Value> mydata(v8::String::New("hello"));
     v8::V8::AddMessageListener(ErrorCallback, mydata);
 
-    // Create context and make it the current scope
-    v8::Handle<v8::Context> context = v8::Context::New();
+    v8::TryCatch try_catch;
+
+    // Create a template for the global object and
+    // add the function template for print to it.
+    v8::Handle<v8::ObjectTemplate> global = v8::ObjectTemplate::New();
+    global->Set(v8::String::New("print"), v8::FunctionTemplate::New(Print));
+
+    // Create context with our globals and make it the current scope
+    v8::Handle<v8::Context> context = v8::Context::New(NULL, global);
     v8::Context::Scope context_scope(context);
 
     // Compile the source
-    v8::Handle<v8::String> source = v8::String::New("\n\n    'Hi\n\n");
+    v8::Handle<v8::String> source = v8::String::New(
+        "function onRequest(reqNum) {\n"
+        "  print(\"reqNum=\" + reqNum);\n"
+        "  return \"Hello World\";\n"
+        "}\n");
     v8::Handle<v8::Script> script = v8::Script::Compile(source);
     if (script.IsEmpty()) {
+        LOGD("testV8 compile source failed");
         ReportException(&try_catch);
     } else {
         // Run the resulting script
         v8::Handle<v8::Value> result = script->Run();
-        if (result.IsEmpty()) {
+        if (try_catch.HasCaught()) {
+            LOGD("testV8 run script failed");
             ReportException(&try_catch);
         } else {
-            v8::String::Utf8Value result_string(result);
-            LOGD("testV8 result=%s", ToCString(result_string));
+            // Call the onRequest function
+            callOnRequest(context, 61);
         }
     }
     LOGD("testV8 X:");
 }
 
 pthread_t s_tid_mainloop;
+static void * mainLoop(void *param)
+{
+    // Test using STLport
+    std::queue<void *> q;
+
+    LOGD("before push q.size=%d", q.size());
+    q.push(param);
+    LOGD("after push q.size=%d", q.size());
+    void *p = q.front();
+    if (p == param) {
+        LOGD("q.push succeeded");
+    } else {
+        LOGD("q.push failed");
+    }
+    q.pop();
+    LOGD("after pop q.size=%d", q.size());
+
+    // Test a simple protobuf
+    LOGD("create T a simple protobuf");
+    T* t = new T();
+    LOGD("set_id(1)");
+    t->set_id(1);
+    int id = t->id();
+    LOGD("id=%d", id);
+    delete t;
+
+    LOGD("mainLoop E");
+
+    for (int i=0;;i++) {
+        sleep(10);
+        LOGD("mainLoop: looping %d", i);
+    }
+
+    LOGD("mainLoop X");
+
+    return NULL;
+}
 
 const RIL_RadioFunctions *RIL_Init(const struct RIL_Env *env, int argc, char **argv)
 {
@@ -251,6 +333,7 @@ const RIL_RadioFunctions *RIL_Init(const struct RIL_Env *env, int argc, char **a
 
     LOGD("RIL_Init E");
 
+    v8Bug1();
     testV8();
 
     s_rilenv = env;
