0d9f0c0ea45a0239653e04f2c63ccecb1bc62c6b
Dianne Hackborn <hackbod@google.com>
2010-06-25 16:50:46 -0700

    Revert "am 9a2f5113: merge from open-source master"
    This reverts commit bb1375f6bdc6cf7d8c3642367cf35565ddb094a8, reversing
    changes made to ca5e347ce4b2b51385c4b7395d51c30498c41a6b.

--- include/telephony/ril.h
+++ include/telephony/ril.h
@@ -3481,14 +3481,8 @@ struct RIL_Env {
      * soon as possible
      */
 
-    void* (*RequestTimedCallback) (RIL_TimedCallback callback,
+    void (*RequestTimedCallback) (RIL_TimedCallback callback,
                                    void *param, const struct timeval *relativeTime);
-
-
-    /**
-     * Remove user-specified callback function
-     */
-    void (*RemoveTimedCallback) (void *callbackInfo);
 };
 
 
@@ -3552,15 +3546,8 @@ void RIL_onUnsolicitedResponse(int unsolResponse, const void *data,
  * @param relativeTime a relative time value at which the callback is invoked
  */
 
-void* RIL_requestTimedCallback (RIL_TimedCallback callback,
-                                void *param, const struct timeval *relativeTime);
-
-/**
- * Remove user-specified callback function
- *  @param callbackInfo  Pointer returned to the caller when timer was scheduled
- */
-
-void RIL_removeTimedCallback(void *callbackInfo);
+void RIL_requestTimedCallback (RIL_TimedCallback callback,
+                               void *param, const struct timeval *relativeTime);
 
 
 #endif /* RIL_SHLIB */
