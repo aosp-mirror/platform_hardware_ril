e18b85ca88ae5fa67f4d0c8698ceaa64cfda16de
Ivan Krasin <krasin@google.com>
2016-01-27 11:17:58 -0800

    Subject: [PATCH] Fix heap-use-after-free in reference-ril.c found by AddressSaniziter.
    BUG=26012424
    
    (cherry picked from commit 7c0165ee3b769ed6a67b58b7c6e2f60bd6f8fbf6)
    
    Change-Id: I108f01229b5fe6a368129e608195ba5833f2ea91

--- reference-ril/reference-ril.c
+++ reference-ril/reference-ril.c
@@ -3110,7 +3110,6 @@ static void onUnsolicited (const char *s, const char *sms_pdu)
 
         err = at_tok_nextstr(&p, &response);
 
-        free(line);
         if (err != 0) {
             RLOGE("invalid NITZ line %s\n", s);
         } else {
@@ -3118,6 +3117,7 @@ static void onUnsolicited (const char *s, const char *sms_pdu)
                 RIL_UNSOL_NITZ_TIME_RECEIVED,
                 response, strlen(response));
         }
+        free(line);
     } else if (strStartsWith(s,"+CRING:")
                 || strStartsWith(s,"RING")
                 || strStartsWith(s,"NO CARRIER")
