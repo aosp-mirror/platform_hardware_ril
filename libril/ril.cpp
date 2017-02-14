abd7275b037423a7425c00f0c70f452e5cbc498f
Amit Mahajan <amitmahajan@google.com>
2017-02-13 17:43:04 -0800

    OemHook service implementation.
    Test: Basic telephony sanity
    Bug: 32020264
    Bug: 34344851
    Change-Id: Ie165921c02b301b378f8f05eeec24a2f273691f4

--- libril/ril.cpp
+++ libril/ril.cpp
@@ -4522,7 +4522,6 @@ RIL_onRequestAck(RIL_Token t) {
         // unsolicited commands present in other CL
         int cmdId = pRI->pCI->requestNumber;
         if(cmdId != RIL_REQUEST_SETUP_DATA_CALL
-                && cmdId != RIL_REQUEST_OEM_HOOK_RAW
                 && cmdId != RIL_REQUEST_SET_INITIAL_ATTACH_APN
                 && cmdId != RIL_REQUEST_SET_DATA_PROFILE) {
             pthread_rwlock_t *radioServiceRwlockPtr = radio::getRadioServiceRwlock(
@@ -4599,7 +4598,6 @@ RIL_onRequestComplete(RIL_Token t, RIL_Errno e, void *response, size_t responsel
         bool hidlized = false;
         int cmdId = pRI->pCI->requestNumber;
         if(cmdId != RIL_REQUEST_SETUP_DATA_CALL
-                && cmdId != RIL_REQUEST_OEM_HOOK_RAW
                 && cmdId != RIL_REQUEST_SET_INITIAL_ATTACH_APN
                 && cmdId != RIL_REQUEST_SET_DATA_PROFILE) {
             hidlized = true;
