4799b529349e229cba4c2d5fe48454a8b3d32d09
bohu <bohu@google.com>
2017-02-28 14:55:24 -0800

    Emulator: adjust reference-ril to the refactored qemu_pipe
    qemu_pipe is refactored into a library in system/core/qemu_pipe,
    and reference-ril needs to adjust accordingly.
    
    Change-Id: I13160ce6a07a721501fd893dd9c5a9931927e767
    (cherry picked from aosp commit 7b7267ac17fa6d557a762f23cb5b7e97de21372f)

--- reference-ril/Android.mk
+++ reference-ril/Android.mk
@@ -14,10 +14,12 @@ LOCAL_SRC_FILES:= \
 LOCAL_SHARED_LIBRARIES := \
     liblog libcutils libutils libril librilutils
 
+LOCAL_STATIC_LIBRARIES := libqemu_pipe libbase
+
 # for asprinf
 LOCAL_CFLAGS := -D_GNU_SOURCE
 
-LOCAL_C_INCLUDES :=
+LOCAL_C_INCLUDES := system/core/qemu_pipe/include
 
 ifeq ($(TARGET_DEVICE),sooner)
   LOCAL_CFLAGS += -DUSE_TI_COMMANDS
