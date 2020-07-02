3c1f1239ad077199a9c0e263e39dbe940d516034
jinhong.seol <jinhong.seol@samsung.com>
2020-07-02 17:03:58 +0900

    change parameter type for 32bit platform
    Signed-off-by: jinhong.seol <jinhong.seol@samsung.com>
    Change-Id: I8df3b31c8dd0bff92489507b52db7a30643b4eea

--- libril/ril_service.h
+++ libril/ril_service.h
@@ -743,7 +743,7 @@ int carrierInfoForImsiEncryption(int slotId,
 
 pthread_rwlock_t * getRadioServiceRwlock(int slotId);
 
-void setNitzTimeReceived(int slotId, long timeReceived);
+void setNitzTimeReceived(int slotId, int64_t timeReceived);
 
 }   // namespace radio
 
